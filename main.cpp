6f761ab8a25dbd645235af113b3e1289b334f649
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-16 13:44:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
+#include <math.h>
+#include <iostream>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-  return 0;
+#include "user.h"
+//#include "print.h"
+
+
+using namespace std;
+
+int main() {
+  int a[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+  int b[5] = {1,2,3,4,5};
+  user user1 (a,b);
+  cout << "hello" << endl;
+
 }
