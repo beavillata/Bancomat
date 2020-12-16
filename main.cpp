bb1e5f969b665b112aa968b68437eeb685eb47d8
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-16 14:39:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,25 @@
 #include <stdlib.h>
 
 #include "user.h"
-//#include "print.h"
+#include "login.h"
 
 
 using namespace std;
 
 int main() {
-  int a[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-  int b[5] = {1,2,3,4,5};
-  user user1 (a,b);
-  cout << "hello" << endl;
+  //DATABASE
+  int tizioNC[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+  int tizioPIN[5] = {1,2,3,4,5};
+
+  //Tizio LOGIN
+  int inputNC[13] = {1,2,4,4,5,6,7,8,9,10,11,12,13};
+  int inputPIN[5] = {1,2,3,4,5};
+
+  user tizio (inputNC,inputPIN);
+
+  login prova (tizio, tizioNC, tizioPIN);
+
+  prova.CNC();
+
 
 }
