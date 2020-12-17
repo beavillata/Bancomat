82f573ed7c5fcfbe1cc6d69fafca253c28092967
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-17 12:27:40 +0100

    Update user.cpp

--- user.cpp
+++ user.cpp
@@ -9,31 +9,31 @@ using namespace std;
 user::user(int nc[], int pin[])
 {
   for(int i=0;i<13;i++)
-  {numerocarta[i]=nc[i];}
+  {numerocarta[i]=&nc[i];}
   for(int i=0;i<5;i++)
-  {PIN[i]=pin[i];}
+  {PIN[i]=&pin[i];}
 }
 
 void user::setNC(int nc[])
 {
   for(int i=0;i<13;i++)
-  {numerocarta[i]=nc[i];}
+  {numerocarta[i]=&nc[i];}
 }
 
 void user::setPIN(int pin[])
 {
   for(int i=0;i<5;i++)
-  {PIN[i]=pin[i];}
+  {PIN[i]=&pin[i];}
 }
 
 int* user::getNC()
 {
-  return numerocarta;
+  return *numerocarta;
 }
 
 int* user::getPIN()
 {
-  return PIN;
+  return *PIN;
 }
 
 //void punto::stampa()
