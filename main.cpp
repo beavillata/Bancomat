ca9102712328a203d54798a3910956c17c133b27
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-17 14:39:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,35 +12,37 @@ int main() {
   //DATABASE
   int tizioNC[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
   int tizioPIN[5] = {1,2,3,4,5};
+  double tizioSALDO = 1000.2;
+  //std::vector<int> v; //movimenti (??)
 
   //Tizio LOGIN
-  int inputNC[13] = {1,2,4,4,5,6,7,8,9,10,11,22,13};
+  int inputNC[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
   int inputPIN[5] = {1,2,3,4,5};
 
   user tizio (tizioNC,tizioPIN);
 
-  //PROVA LOGIN TIZIO
 
+  login prova2 (tizio.getNC(), tizio.getPIN(), inputNC, inputPIN);
+  if(prova2.accesso(prova2)==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
+
+  //PROVE LOGIN TIZIO:
+/*
   cout << "Tentativo 1: " << endl;
   //numero di carta sbagliato
-  login prova1 (tizio.getNC(),tizio.getPIN(), inputNC, inputPIN);
-  prova1.CNC();
-  prova1.CPIN();
-  if(prova1.accesso()==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
+  int inputNC2[13] = {1,2,3,5,5,6,7,8,9,10,11,12,13};
+  login prova1 (tizio.getNC2(),tizio.getPIN(), inputNC, inputPIN);
+  if(prova1.accesso(prova1)==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
 
   cout << "Tentativo 2:" << endl;
   //numero di carta giusto, pin giusto
-  int inputNC2[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
   login prova2 (tizio.getNC(), tizio.getPIN(), inputNC2, inputPIN);
-  prova2.CNC();
-  prova2.CPIN();
-  if(prova2.accesso()==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
+  if(prova2.accesso(prova2)==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
 
   cout << "Tentativo 3:" << endl;
   //numero di carta giusto, pin sbagliato
   int inputPIN2[5] = {1,2,4,4,5};
   login prova3 (tizio.getNC(), tizio.getPIN(), inputNC2, inputPIN2);
-  prova3.CNC();
-  prova3.CPIN();
-  if(prova3.accesso()==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
+  if(prova3.accesso(prova3)==true){cout << "ACCESSO ESEGUITO" << "\n" << endl;}
+*/
+
 }
