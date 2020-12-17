279e96c19c0419dbfb5e3abedfe3153185dc5d9e
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-17 12:27:48 +0100

    Update login.cpp

--- login.cpp
+++ login.cpp
@@ -7,23 +7,78 @@
 
 using namespace std;
 
-login::login(user &u, int cnc[], int cpin[])
+login::login(int *cnc, int *cpin, int *icn, int *ipin)
 {
-  for(int i=0;i<13;i++)
-  {checkNC[i]=cnc[i];}
-  for(int i=0;i<5;i++)
-  {checkPIN[i]=cpin[i];}
-  USR=u;
+  for(int i=0; i<13; i++)
+  {
+    checkNC[i]=cnc[i];
+    inputNC[i]=icn[i];
+  }
+  for(int i=0; i<5; i++)
+  {
+    checkPIN[i]=cpin[i];
+    inputPIN[i]=ipin[i];
+  }
+  checkbool=true;
 }
 
+//controllo se il numero di carta è giusto
 void login::CNC()
 {
-  bool check = true;
-  for(int i=0;i<13;i++)
+  checkbool=true;
+  int i=0;
+  bool condition=true;
+  while(condition==true)
   {
-    if (USR.getNC()[i] == checkNC[i]){}
-    else {check=false;}
+    if(checkNC[i]==inputNC[i]){}
+    else
+    {
+      checkbool=false;
+      condition=false;
+      cout << "Numero di Carta ERRATO!" << endl;
+    }
+    i=i+1;
+    if(i==13){condition=false;}
   }
-  if (check == false) {cout << "numero di carta errato" << endl;}
-  else {cout << "..." << endl;}
+  if(checkbool==true){cout << "Numero di Carta Corretto" << endl;}
 }
+
+void login::CPIN()
+{
+  int i=0;
+  bool condition=true;
+  if(checkbool == false){cout << "" << endl;}
+  else
+  {
+    while(condition==true)
+    {
+      if(checkPIN[i]==inputPIN[i]){}
+      else
+      {
+        checkbool=false;
+        condition=false;
+        cout << "PIN ERRATO!" << endl;
+      }
+      i=i+1;
+      if(i==5){condition=false;}
+    }
+    if(checkbool==true){cout << "PIN Corretto" << endl;}
+  }
+}
+//controllo se il pin è giusto
+/*
+void login::CPIN()
+{
+  if (checkbool == false){cout << "" << endl;} //Se il numero di carta è errato NON controllo il pin
+  else{
+    for(int i=0;i<5;i++)
+    {
+      if (USR->getPIN()[i] == *checkPIN[i]){}
+      else {checkbool=false;}
+    }
+    if (checkbool == false) {cout << "PIN errato" << endl;}
+    else {cout << "accesso" << endl;}
+  }
+
+}
+*/
