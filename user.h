44567e125d75a7a122c684bf8e995431bcd587a2
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-17 15:30:13 +0100

    Update user.h

--- user.h
+++ user.h
@@ -4,19 +4,22 @@
 class user{
 public:
   user(){};
-  user(int[],int[]);
+  user(int[],int[],double);
 
   void setNC(int[]);
   void setPIN(int[]);
+  void setSALDO(double);
 
   int* getNC();
   int* getPIN();
+  double getSALDO();
 
   //void stampa();
 
 private:
   int *numerocarta[13];
   int *PIN[5];
+  double SALDO;
 };
 
 #endif
