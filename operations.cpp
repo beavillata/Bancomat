2e637441a6fe69fe4ef5a73502da0fbfe66c9d01
beavillata <60182053+beavillata@users.noreply.github.com>
2020-12-18 14:48:15 +0100

    Update operations.cpp

--- operations.cpp
+++ operations.cpp
@@ -4,15 +4,32 @@
 
 #include "operations.h"
 #include "user.h"
+#include "login.h"
 
 using namespace std;
 
-operations::operations(user &u)
+operations::operations(user &u, bool log)
 {
   *USR=u;
+  LOG=log;
 }
 
 void operations::prelievo(double amount)
 {
-  USR->setSALDO(USR->getSALDO()-amount);
+  if(LOG==true)
+  {
+    if((USR->getSALDO()-amount)<0){cout << "Saldo Insufficente: operazione bloccata" << endl;}
+    else
+    {
+      USR->setSALDO(USR->getSALDO()-amount);
+      cout << "Sono stati prelevati: " << amount << " euro" << endl;
+    }
+  }
+  else{cout << "accesso negato" << endl;}
+}
+
+void operations::versamento(double amount)
+{
+    USR->setSALDO(USR->getSALDO()+amount);
+    cout << "Sono stati versati " << amount << " euro" << endl;
 }
