4c9df0153df921fbf9403479a0e5e3b59a48d1b4
beavillata <60182053+beavillata@users.noreply.github.com>
2021-01-07 13:13:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,13 @@ int main(int argc, char* argv[]) {
       std::cout << "Invalid card number." << std::endl;
       continue;
     }
+    std::cout << "Select the option: \n[1]. Continue [2]. Exit" << std::endl;
+    int option;
+    std::cin >> option;
+    if(option==2) {
+      Login::logout();
+      continue;
+    }
 
     std::cout << "Please input your PIN: ";
     if(!IO::inputPin(pin)) {
